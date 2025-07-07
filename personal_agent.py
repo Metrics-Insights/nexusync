3919cfed6cac34db729f2dd11af37ef2f3c9b933
Andy Wo <zwo@lenovo.com>
2025-07-07 19:47:54 +0900

    add KB https://github.com/MicrosoftDocs/windows-driver-docs

--- personal_agent.py
+++ personal_agent.py
@@ -25,6 +25,7 @@ LOCAL_DB="settings.db"
 TEMPERATURE = 0.4
 INPUT_DIRS = [
     "sample_docs/",
+    "/mnt/d/dev/Metrics-Insights/windows-driver-docs",
     "/mnt/c/Users/ANDY-TP-001/OneDrive - Lenovo/Lenovo TP/Knowledge Base/",
     "/mnt/c/Users/ANDY-TP-001/OneDrive - Lenovo/Microsoft Teams Chat Files/"
 ]  # Can include multiple paths
